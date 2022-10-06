abd24f9e9b42c4e0ec51fcd01301494ac3449cc1
ValF4 <vallkemgamer@gmail.com>
2022-10-05 21:07:50 -0300

    Commit: Organização e adição de função
    Foi feita uma organização no codigo base e adicionado a função de descida da pagina, fazendo assim, a pagina extrair os 60 itens totais da pagina

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 URL = 'https://pt.aliexpress.com'
 
 
@@ -15,23 +16,31 @@ def searchProduct(driver, product_text):
     driver.find_element('xpath', '//*[@id="search-key"]').send_keys(product_text)
     driver.find_element('xpath', '//*[@id="form-searchbar"]/div[1]/input').click()
 
+    for roll in range(5):
+        driver.execute_script('window.scrollTo(0, {});'.format(roll*1000))
+
 
 def getProductsInPage(driver):
     browser = BeautifulSoup(driver.page_source, 'html.parser')
     listProducts = []
-    elements = browser.find('div', attrs={'class': 'JIIxO'}).findAll('a', attrs={'class': '_3t7zg _2f4Ho'})
+    elements = browser.find(
+        'div', attrs={'class': 'JIIxO'}).findAll('a', attrs={'class': '_3t7zg _2f4Ho'})
 
     for element in elements:
-        spans = element.find('div', attrs={'class': 'mGXnE _37W_B'}).findAll('span')
+        spans = element.find(
+            'div', attrs={'class': 'mGXnE _37W_B'}).findAll('span')
+        
         price = ''
+
 
         for span in spans:
             price = price + span.getText()
 
+
         product = Product()
         product.name = element.find('h1').getText()
 
-        # Efetuei a validação, me fale o porque =)
+
         if len(price) > 3:
             product.price = float(price.replace('R$', '').replace('.', '').replace(',', '.'))
             product.priceIsValid = True
@@ -46,7 +55,6 @@ def main():
     browser.get(URL)
     searchProduct(browser, 'redmi')
     listProducts = getProductsInPage(browser)
-    # TODO: Salvar os elementos no banco de dados
 
 
 if __name__ == "__main__":
