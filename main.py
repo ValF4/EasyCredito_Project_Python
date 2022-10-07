262fd4d7249bc5e7a25d3afbfe1d3ade833d3b50
ValF4 <vallkemgamer@gmail.com>
2022-10-07 13:48:01 -0300

    organização e remoção de bug

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ URL_Main = 'https://pt.aliexpress.com'
 class Product:
     name = ''
     price = 0.0
+    url = ''
     priceIsValid = False
 
 # Função Main(Principal)
@@ -27,16 +28,15 @@ def main():
 def scroll_down(driver):
     for roll in range(5):
         driver.execute_script('window.scrollTo(0, {});'.format(roll*1000))
-
+''
 # Raspando as informações
 def getItensInPage(driver):
     listProducts = []
-    price= ''
     page = BeautifulSoup(driver.page_source, 'html.parser')
     elements = page.find('div', attrs={'class': 'JIIxO'}).find_all('a', attrs={'class': '_3t7zg _2f4Ho'})
 
     for element in elements:
-        
+        price= ''        
         price_products = element.find('div', attrs={'class': 'mGXnE _37W_B'}).find_all('span')
         
         for name in price_products: ## Extraindo preço
@@ -44,13 +44,13 @@ def getItensInPage(driver):
 
         product = Product() ## Extraindo Nome
         product.name = element.find('h1').getText()
+        product.url = element.find('href')
 
-        if len(price_products) > 3:
+        if len(price) > 3:
             product.price = float(price.replace('R$', '').replace('.', '').replace(',', '.'))
             product.priceIsValid = True
 
-        listProducts.append(product.price)
-        listProducts.append(product.name)
+        listProducts.append([product.name] [product.price])
  
     return listProducts
 
